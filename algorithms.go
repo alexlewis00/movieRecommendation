37bc5fb322dc3d19a9c4daa34ed7eac4c85dcc1a
Alex Lewis <alewis2@scu.edu>
2021-03-09 10:48:35 -0800

    Added some golang programming notes
    Added notes to get a better sense of how to write code with golang

--- algorithms.go
+++ algorithms.go
@@ -2,9 +2,39 @@
 package main
 
 //fmt standard library package includes functions for formatting text like printing to the console
-import "fmt"
+import (
+	"fmt"
+)
+
+//Functions that return something need that type specified at end
+	//fun sum(x int, y int) int {}
 
 //Main function, executes when running the main package
+//run file in terminal with "go run <filename>" command
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Testing...")
+
+	/*
+	//Declaring a variable
+	//<var keyword> <name of variable> <type of variable> = default value is 0 for respective variable type
+		var x int
+	//other way to declare a variable with value
+		x := 5 
+
+	//Slices allow for dynamic arrays, no fixed length
+		array := []int{1, 2, 3, 4}
+
+	//maps hold key value pairs
+		//map[type of keys]<type of values in the map>
+		//create map with built in make function
+		vertices := make(map[string]int)
+
+		//set key value pairs with []
+		vertices["first"] = 1
+		vertices["second"] = 2
+		//Results in: map[first:1 second:2]
+
+	//Traverse thorugh an array or slice with range in for loop
+		for index, value := range arr {"index:", index, "value:", value}
+	*/
 }
