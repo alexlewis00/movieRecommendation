74f79a5d2baccd72a9b0b1af81e4c19689db5912
Alex Lewis <alewis2@scu.edu>
2021-03-09 10:07:34 -0800

    Setting up golang
    Set up golang module with dependency to github movieRecommendation repository, and created first go file with package main to run main function.

---
+++ algorithms.go
@@ -0,0 +1,10 @@
+//Declaring main package, groups functions and all files in same directory movieRecommendation
+package main
+
+//fmt standard library package includes functions for formatting text like printing to the console
+import "fmt"
+
+//Main function, executes when running the main package
+func main() {
+	fmt.Println("Hello, World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexlewis00/movieRecommendation
+
+go 1.16
