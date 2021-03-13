bc90429974d4e86a454c0bd3b1cace09c34599d1
Alex Lewis <alewis2@scu.edu>
2021-03-12 16:54:46 -0800

    Updated pearsonIUF
    returned predictions are out of range of 1-5, need to figure out the calculations for the IUF multiplier to the ratings data.

--- pearsonIUF.go
+++ pearsonIUF.go
@@ -91,26 +91,132 @@ func getTestData(filename string) [100][1000]int {
 	return users
 }
 
+//Function to read the test data passed as an argument and insert data into 2d array (200 users x 1000 movies) multiplied by IUF value
+func getDataIUF() [200][1000]float64 {
+	data, err := ioutil.ReadFile("../Data/train.txt") //read contents of file txt into data array
+	if err != nil {
+		fmt.Println("Failed to read training file")
+		return [200][1000]float64{}
+	}
+	//scan input as sequence of space-delimited tokens
+	scanner := bufio.NewScanner(strings.NewReader(string(data)))
+	scanner.Split(bufio.ScanWords) //split function for only scanning words, not spaces
+	buffer := [200][1000]int{}     //buffer to store data in
+
+	for row := 0; row < 200; row++ {
+		for col := 0; col < 1000; col++ {
+			scanner.Scan()                                   //advances scanner to next token
+			currentRank, err := strconv.Atoi(scanner.Text()) //Atoi: string conversion to int
+			if err != nil {
+				fmt.Println("Error with scanner at token")
+				return [200][1000]float64{}
+			}
+			buffer[row][col] = currentRank //adding integer value pos into buffer at specified position
+		}
+	}
+	var updatedTrainData[200][1000] float64
+	//traverse through movies to see how many users have rated that movie
+	for movie := 0; movie < 1000; movie++ {
+		var numRated int = 0 //represents mj in IUF equation
+		for user := 0; user < 200; user++ {
+			if buffer[user][movie] != 0 {
+					numRated++
+			}
+		}
+		//number of users that have rated that movie is collected
+		//execute IUF calculation and multiply original ratings
+		for user := 0; user < 200; user++ {
+			if buffer[user][movie] != 0 { //there is a rating present that needs to be adjusted
+				updatedTrainData[user][movie] = float64(buffer[user][movie]) * (math.Log(float64(200/numRated)))
+			}
+		}
+	}
+	return updatedTrainData
+}
+
+//Function to read the test data passed as an argument and insert data into 2d array (100 users x 1000 movies) multiplied by IUF value
+func getTestDataIUF(filename string) [100][1000]float64 {
+	data, err := ioutil.ReadFile(filename) //read contents of text file into data array
+	if err != nil {
+		fmt.Println("Failed to read test file")
+		return [100][1000]float64{}
+	}
+	scanner := bufio.NewScanner(strings.NewReader(string(data))) //scan input as string of space-delimited tokens
+	scanner.Split(bufio.ScanLines) //split function for only scanning words, not spaces
+	checkUsers := []int{} //slice to store userid's for checking
+	var checker int
+	users := [100][1000]int{} //100 users starting at userid 0, 1000 movies starting at movieid 0
+	//ensure only the movies for each user as given by the test data applies
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 1000; j++ {
+			users[i][j] = 9
+		}
+	}
+	for scanner.Scan() {
+		line := strings.Fields(scanner.Text()) //Fields function breaks a string around each instance of white space into an array
+		currentUser := line[0]
+		userId, _ := strconv.Atoi(currentUser) //Atoi: string conversion to int
+		currentMovie := line[1]
+		movieId, _ := strconv.Atoi(currentMovie)
+		currentRating := line[2]
+		rating, _ := strconv.Atoi(currentRating)
+		//for first instance of test user
+		if len(checkUsers) == 0 {
+			checkUsers = append(checkUsers, userId)
+		} else {
+			checker = 0
+			for i := 0; i < len(checkUsers); i++ { //traverse slice to ensure there are no duplicates for userid
+				if checkUsers[i] == userId {
+					checker = 1
+				}
+			}
+			if checker == 0 { //adding new userid to users array
+				checkUsers = append(checkUsers, userId)
+			}
+		}
+		//populate 2d array
+		users[len(checkUsers)-1][movieId-1] = rating
+	}
+	var updatedTestData[100][1000] float64
+	//traverse through movies to see how many users have rated that movie
+	for movie := 0; movie < 1000; movie++ {
+		var numRated int = 0 //represents mj in IUF equation
+		for user := 0; user < 100; user++ {
+			if users[user][movie] != 0 && users[user][movie] != 9 {
+					numRated++
+			}
+		}
+		//number of users that have rated that movie is collected
+		//execute IUF calculation and multiply original ratings
+		for user := 0; user < 100; user++ {
+			if users[user][movie] != 0 && users[user][movie] != 9 { //there is a rating present that needs to be adjusted
+				updatedTestData[user][movie] = float64(users[user][movie]) * (math.Log(float64(200/numRated)))
+			}
+		}
+	}
+	return updatedTestData
+}
+
 //Function to find the pearson similarity between two users
 func userPearson(userOne int, userTwo int, filename string) float64 {
-	trainingData := getData() //Get the training data
-	testData := getTestData(filename)
+	testDataIUF := getTestDataIUF(filename)
+	trainingDataIUF := getDataIUF()
 	train := "train"
-	var numerator int = 0
-	var denominator float64 = 0
-	var squaredOne int = 0
-	var squaredTwo int = 0
-	var similarity float64 = 0
+	var numerator float64 = 0.0
+	var denominator float64 = 0.0
+	var squaredOne float64 = 0.0
+	var squaredTwo float64 = 0.0
+	var similarity float64 = 0.0
 	activeUserAverage := getAverageRating(userOne, filename)
 	otherUserAverage := getAverageRating(userTwo, train)
 
 	//traverse through the movie set to find the movies userOne and userTwo have rated
 	for movieId := 0; movieId < 1000; movieId++ {
-		if testData[userOne][movieId] != 0 && testData[userOne][movieId] != 9 && trainingData[userTwo][movieId] != 0 { //both users have rated the same movie
-			//determine similarity score between users
-			numerator = numerator + ((testData[userOne][movieId] - activeUserAverage) * (trainingData[userTwo][movieId] - otherUserAverage))
-			squaredOne = squaredOne + ((testData[userOne][movieId] - activeUserAverage) * (testData[userOne][movieId] - activeUserAverage))
-			squaredTwo = squaredTwo + ((trainingData[userTwo][movieId] - otherUserAverage) * (trainingData[userTwo][movieId] - otherUserAverage))
+		if testDataIUF[userOne][movieId] != 0 && testDataIUF[userOne][movieId] != 9 && trainingDataIUF[userTwo][movieId] != 0 { //both users have rated the same movie
+			//determine similarity score between users with the new IUF ratings
+			numerator = numerator + ((testDataIUF[userOne][movieId] - activeUserAverage) * (trainingDataIUF[userTwo][movieId] - otherUserAverage))
+			squaredOne = squaredOne + ((testDataIUF[userOne][movieId] - activeUserAverage) * (testDataIUF[userOne][movieId] - activeUserAverage))
+			squaredTwo = squaredTwo + ((trainingDataIUF[userTwo][movieId] - otherUserAverage) * (trainingDataIUF[userTwo][movieId] - otherUserAverage))
 		}
 	}
 	denominator = math.Sqrt(float64(squaredOne)) * math.Sqrt(float64(squaredTwo))
@@ -118,29 +224,29 @@ func userPearson(userOne int, userTwo int, filename string) float64 {
 	return similarity
 }
 
-func getAverageRating(user int, filename string) int {
+func getAverageRating(user int, filename string) float64 {
 	var sum float64
 	var num int
 
 	//for the other user
 	if filename == "train" {
-		trainData := getData()
+		trainData := getDataIUF()
 		for movie := 0; movie < 1000; movie++ {
 			if trainData[user][movie] != 0 { //user has rated that movie
 				sum += float64(trainData[user][movie])
 				num++
 			}
 		}
-		return int(sum / float64(num)) //returns average rating for given user
+		return sum / float64(num) //returns average rating for given user
 	} else { //for the active user
-		testData := getTestData(filename)
+		testData := getTestDataIUF(filename)
 		for movie := 0; movie < 1000; movie++ {
 			if testData[user][movie] != 0 && testData[user][movie] != 9 { //user has rated that movie
 				sum += float64(testData[user][movie])
 				num++
 			}
 		}
-		return int(sum / float64(num))
+		return sum / float64(num)
 	}
 }
 
@@ -176,7 +282,7 @@ func getPrediction(activeUser int, activeMovie int, filename string) int {
 	
 	for u := 0; u < 15; u++ { //traverse through the top 15 most similar users
 		otherUserAverage := getAverageRating(u, train)
-		numerator = numerator + (kSimilarRatings[u] * (float64(trainingData[kSimilarUsers[u]][activeMovie] - otherUserAverage)))
+		numerator = numerator + (kSimilarRatings[u] * (float64(trainingData[kSimilarUsers[u]][activeMovie] - int(otherUserAverage))))
 		denominator = denominator + math.Abs(kSimilarRatings[u])
 	}
 
